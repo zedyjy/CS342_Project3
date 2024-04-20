32faeb4f35b915872651af23852b96d66c6c0184
alperen <ahmetalpyy@gmail.com>
2024-04-20 22:14:45 +0300

    makefile

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC	:= gcc
 CFLAGS := -g -Wall
 
-TARGETS :=  libmf.a  app  producer consumer mfserver
+TARGETS :=  libmf.a  app app2 app3 producer consumer mfserver
 
 # Make sure that 'all' is the first target
 all: $(TARGETS)
@@ -22,6 +22,18 @@ app.o: app.c  mf.c mf.h
 
 app: app.o libmf.a mf.o
 	gcc $(CFLAGS) -o $@ app.o $(MF_LIB)
+
+app2.o: app2.c  mf.c mf.h
+	gcc -c $(CFLAGS)  -o $@ app2.c
+
+app2: app2.o libmf.a mf.o
+	gcc $(CFLAGS) -o $@ app2.o $(MF_LIB)
+
+app3.o: app3.c  mf.c mf.h
+	gcc -c $(CFLAGS)  -o $@ app3.c
+
+app3: app3.o libmf.a mf.o
+	gcc $(CFLAGS) -o $@ app3.o $(MF_LIB)
 
 producer.o: producer.c  mf.c mf.h
 	gcc -c $(CFLAGS)  -o $@ producer.c
@@ -46,4 +58,4 @@ test: test.c
 	gcc -g -Wall  -o  test test.c
 
 clean:
-	rm -rf core  *.o *.out *~ $(TARGETS) app1 app1-2 app2 app3 producer consumer
+	rm -rf core  *.o *.out *~ $(TARGETS) app1 app2 app3 producer consumer
--- app.c
+++ app.c
@@ -8,39 +8,47 @@
 #include <semaphore.h>
 #include <sys/mman.h>
 #include <time.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include "mf.h"
 
 #define COUNT 10
-
+char *semname1 = "/semaphore1";
+char *semname2 = "/semaphore2";
+sem_t *sem1, *sem2;
 char *mqname1 = "msgqueue1";
 
 int
 main(int argc, char **argv)
 {
-    int ret, qid;
+    int ret,  i, qid;
     char sendbuffer[MAX_DATALEN];
     int n_sent, n_received;
     char recvbuffer[MAX_DATALEN];
-    int sentcount = 0;
-    int receivedcount = 0;
+    int sentcount;
+    int receivedcount;
     int totalcount;
 
     totalcount = COUNT;
     if (argc == 2)
         totalcount = atoi(argv[1]);
 
-    srand(time(0));
+    sem1 = sem_open(semname1, O_CREAT, 0666, 0); // init sem
+    sem2 = sem_open(semname2, O_CREAT, 0666, 0); // init sem
 
-    mf_connect();
-    mf_create (mqname1, 16); //  create mq;  16 KB
+    srand(time(0));
+    printf ("RAND_MAX is %d\n", RAND_MAX);
 
     ret = fork();
     if (ret > 0) {
         // parent process - P1
+        // parent will create a message queue
+
+        mf_connect();
+
+        mf_create (mqname1, 16); //  create mq;  16 KB
 
         qid = mf_open(mqname1);
+
+        sem_post (sem1);
 
         while (1) {
             n_sent = 1 + (rand() % MAX_DATALEN);
@@ -51,18 +59,22 @@ main(int argc, char **argv)
                 break;
         }
         mf_close(qid);
-
-        wait(NULL); // wait for child to terminate
+        sem_wait(sem2);
+        // we are sure other process received the messages
 
         mf_remove(mqname1);   // remove mq
         mf_disconnect();
-        exit(0);
     }
     else if (ret == 0) {
         // child process - P2
+        // child will connect, open mq, use mq
+        sem_wait (sem1);
+        // we are sure mq was created
 
         mf_connect();
+
         qid = mf_open(mqname1);
+
         while (1) {
             n_received =  mf_recv (qid, (void *) recvbuffer, MAX_DATALEN);
             printf ("app received message, datalen=%d\n", n_received);
@@ -72,7 +84,7 @@ main(int argc, char **argv)
         }
         mf_close(qid);
         mf_disconnect();
-        exit(0);
+        sem_post(sem2);
     }
     return 0;
 }
